553bbd8f3bbf3775e069dcb44e52a87b68544366
jidicula <johanan.idicula@gmail.com>
2018-07-03 18:49:11 -0400

    glob sorting
    Apparently glob doesn't sort...
    
    See
    https://stackoverflow.com/questions/6773584/how-is-pythons-glob-glob-ordered
    for details.

--- Main.py
+++ Main.py
@@ -24,6 +24,7 @@ exp_loc = "/home/jidicula/johanan/prog/test/Mark_and_Find_001"
 nuc_channel = "ch01"
 n_frames = 71
 positions = glob.glob(exp_loc + '/Position*')  # list of full filepaths
+positions.sort()
 n_pos = len(positions)
 print(positions[0])
 first_time = mu.get_time(positions[0] + "/Metadata/Position001_Properties.xml",
