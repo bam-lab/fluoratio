d04c091fa9c0d60257ade04b373b10374cc15e92
jidicula <johanan.idicula@gmail.com>
2018-07-05 10:17:36 -0400

    too intense! reducing loop size :()

--- proc_optimum.py
+++ proc_optimum.py
@@ -16,7 +16,7 @@ for j in range(cpu_num):
         start = time.time()
         if __name__ == '__main__':
             with mp.Pool(processes=(j+1)) as pool:
-                pool.map(doer, range(1000000000))
+                pool.map(doer, range(10000000))
         end = time.time()
         print("Trial {}:".format(i+1), str(end-start), "seconds")
         times.append(end-start)
