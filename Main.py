0465ab3b94b8f44be5783dbe9483fc54e98f70c1
jidicula <johanan.idicula@gmail.com>
2018-07-06 00:14:15 -0400

    back to analysis!

--- Main.py
+++ Main.py
@@ -115,9 +115,9 @@ for k, filepath in enumerate(img_filepaths):
     img_filepaths[k] = new_filepath
 
 # Making the process worker pool.
-# if __name__ == '__main__':
-#     with mp.Pool(processes=(cpu_num)) as pool:
-#         pool.map(analyzer, img_filepaths)
+if __name__ == '__main__':
+    with mp.Pool(processes=(cpu_num)) as pool:
+        pool.map(analyzer, img_filepaths)
 
 # Coalesce all the result csv files into one
 print("hello there")
